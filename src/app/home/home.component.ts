7505c6c1543257c7b3ff25d20be406364462dbb7
Dani <daniela.gavilan05@gmail.com>
2022-05-28 20:46:50 -0300

    agregando sector herramientas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  //declarando url iconos para usar en el html
+
   constructor() { }
 
   ngOnInit(): void {
