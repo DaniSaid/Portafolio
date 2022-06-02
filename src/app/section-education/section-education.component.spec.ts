1b75ea9953dcaaa595626121bca683716ebed8a3
Dani <daniela.gavilan05@gmail.com>
2022-06-02 17:25:23 -0300

    secciones de pagina creadas

---
+++ src/app/section-education/section-education.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SectionEducationComponent } from './section-education.component';
+
+describe('SectionEducationComponent', () => {
+  let component: SectionEducationComponent;
+  let fixture: ComponentFixture<SectionEducationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SectionEducationComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SectionEducationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/section-education/section-education.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-section-education',
+  templateUrl: './section-education.component.html',
+  styleUrls: ['./section-education.component.scss']
+})
+export class SectionEducationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
