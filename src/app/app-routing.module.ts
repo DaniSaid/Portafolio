14dd88b47efb1ec60583b3ae2337156678969041
Dani <daniela.gavilan05@gmail.com>
2022-06-11 18:22:26 -0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './login/login.component';
 import { Pagina404Component } from './pagina404/pagina404.component';
 import { AboutComponent } from './about/about.component';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { LoginButtonComponent} from './login-button/login-button.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { AboutButtonComponent } from './about-button/about-button.component';
 import { LoginComponent } from './login/login.component';
 import { Pagina404Component } from './pagina404/pagina404.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
-import { SectionToolsComponent } from './section-tools/section-tools.component';
+import { SectionToolsComponent } from './components/section-tools/section-tools.component';
 import { HttpClientModule } from '@angular/common/http';
-import { SectionContactComponent } from './section-contact/section-contact.component';
+import { SectionContactComponent } from './components/section-contact/section-contact.component';
 import { AboutComponent } from './about/about.component';
 import { SectionEducationComponent } from './section-education/section-education.component';
 import { SectionExperienceComponent } from './section-experience/section-experience.component';
