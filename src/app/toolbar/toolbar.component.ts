ea7cd6cb3ff7d61ef7a702980a92f1febe4b06cf
Dani <daniela.gavilan05@gmail.com>
2022-06-12 23:05:56 -0300

    adding new toolbar

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit} from '@angular/core';
+import { trigger, state, style, transition, animate} from '@angular/animations';
 import { PortfolioDataService } from 'src/app/services/portfolio-data.service'
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss']
+  styleUrls: ['./toolbar.component.scss'],
+  //animacion del menu
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(100%, 0, 0)'
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ]),
+  ]
 })
 
 export class ToolbarComponent implements OnInit {
-  
-public  icon : string = " /assets/images/b-adjust.svg"
 
-portfolio : any;
+  portfolio : any;
+  
 
   constructor(private portfolioData:PortfolioDataService, public router: Router){
 
@@ -20,12 +33,17 @@ portfolio : any;
   
   ngOnInit(): void {
   
+    //service trae los datos del .json
     this.portfolioData.getData().subscribe(data =>{
       console.log(data);
-    
     this.portfolio = data;
 
     });
   }
-
+  menuState:string = 'out';
+  
+    toggleMenu() {
+      // 1-line if statement that toggles the value:
+      this.menuState = this.menuState === 'out' ? 'in' : 'out';
+    }
 }
